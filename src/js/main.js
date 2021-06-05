914096d37aa10166c380bc8705da6698e4ea74f2
franck <carnet.franck.paul@gmail.com>
2021-06-05 10:46:13 +0200

    Mono header (mobile first)

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,1 @@
 'use strict';
-
-function sleep(time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-window.addEventListener('scroll', function () {
-  let header = document.querySelector('.header');
-  if (window.pageYOffset > 80) {
-    sleep(200).then(() => {
-      header.classList.add('shrink');
-    });
-  } else {
-    sleep(200).then(() => {
-      header.classList.remove('shrink');
-    });
-  }
-});
