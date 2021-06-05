6b706f37b369df47cb0cebea79ecb7984fcef9ba
franck <carnet.franck.paul@gmail.com>
2021-06-05 12:53:21 +0200

    Debounce header changes (every 200ms min)

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,34 @@
 'use strict';
 
-function sleep(time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
+let breakpoint = null;
+let timerId = null;
 
-var breakpoint = null;
+let debounceFunction = function (func, delay) {
+  // Cancels the setTimeout method execution
+  if (timerId) {
+    clearTimeout(timerId);
+  }
+  // Executes the func after delay time.
+  timerId = setTimeout(func, delay);
+};
+
+let doShrinkHeader = function() {
+  let header = document.querySelector('.header');
+  if (breakpoint > 0) {
+    if (window.pageYOffset >= breakpoint) {
+      header.classList.add('shrink');
+      breakpoint = header.offsetHeight;
+    } else if (window.pageYOffset < breakpoint) {
+      header.classList.remove('shrink');
+      breakpoint = header.offsetHeight;
+    }
+  }
+};
 
 document.addEventListener('DOMContentLoaded', function () {
   breakpoint = document.querySelector('.header').offsetHeight;
 });
 
 window.addEventListener('scroll', function () {
-  let header = document.querySelector('.header');
-  if (breakpoint > 0) {
-    if (window.pageYOffset >= breakpoint) {
-      sleep(200).then(() => {
-        header.classList.add('shrink');
-        breakpoint = header.offsetHeight;
-      });
-    } else if (window.pageYOffset < breakpoint) {
-      sleep(200).then(() => {
-        header.classList.remove('shrink');
-        breakpoint = header.offsetHeight;
-      });
-    }
-  }
+  debounceFunction(doShrinkHeader, 200);
 });
