8b9b0a22ff79a6fde5d0707acef147be09b6c47d
franck <carnet.franck.paul@gmail.com>
2021-06-07 10:54:42 +0200

    Add some comments in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let ensemble = {
-  breakpoint: null,
+  breakpoint: null,   // Height of current header, in pixels
   timerId: null,
 
   debounceFunction: function (func, delay) {
@@ -12,23 +12,26 @@ let ensemble = {
   },
 
   doShrinkHeader: function () {
+    // Find blog header
     let header = document.querySelector('.header');
     if (ensemble.breakpoint > 0) {
       if (window.pageYOffset >= ensemble.breakpoint) {
+        // Page scrolled, shrink header
         header.classList.add('shrink');
         ensemble.breakpoint = header.offsetHeight;
       } else if (window.pageYOffset < ensemble.breakpoint) {
+        // Page come back to top, unshrink header
         header.classList.remove('shrink');
         ensemble.breakpoint = header.offsetHeight;
       }
+      // Fix anchor link managment — still buggy in Safari (see https://bugs.webkit.org/show_bug.cgi?id=179379)
+      document.querySelector('html').style.scrollPaddingTop = `${ensemble.breakpoint + 24}px`;
     }
   },
 };
 
-document.addEventListener('DOMContentLoaded', function () {
-  ensemble.breakpoint = document.querySelector('.header').offsetHeight;
-});
+// Get current height of header
+document.addEventListener('DOMContentLoaded', () => ensemble.breakpoint = document.querySelector('.header').offsetHeight);
 
-window.addEventListener('scroll', function () {
-  ensemble.debounceFunction(ensemble.doShrinkHeader, 200);
-});
+// Watch page scrolling and (un)shrink header if necessary
+window.addEventListener('scroll', () => ensemble.debounceFunction(ensemble.doShrinkHeader, 200));
