a19c35e67c0483f060e345e06427625b80a1024c
franck <carnet.franck.paul@gmail.com>
2023-08-20 19:32:08 +0200

    Dotclear 2.27 compliance, PHP 8.1+

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Franck Paul',
     '2.0',
     [
-        'requires' => [['core', '2.26']],
+        'requires' => [['core', '2.27'], ['php', '8.1']],
         'type'     => 'theme',
         'tplset'   => 'dotty',
 
