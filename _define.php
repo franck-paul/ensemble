96b9fba79f8c6140400b8b90d96fef4d8ed07960
franck <contact@open-time.net>
2026-08-13 16:48:43 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -9,20 +9,24 @@
  * @copyright Franck Paul (contact@open-time.net)
  * @copyright GPL-2.0
  */
-$this->registerModule(
-    'Ensemble',
-    'Experimental theme',
-    'Franck Paul',
-    '4.1',
-    [
-        'date'     => '2003-08-13T13:42:00+0100',
-        'requires' => [['core', '2.36']],
-        'type'     => 'theme',
-        'tplset'   => 'dotty',
+declare(strict_types=1);
 
-        'details'    => 'https://open-time.net/?q=ensemble',
-        'support'    => 'https://github.com/franck-paul/ensemble',
-        'repository' => 'https://raw.githubusercontent.com/franck-paul/ensemble/main/dcstore.xml',
-        'license'    => 'gpl2',
-    ]
-);
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'Ensemble',
+        'Experimental theme',
+        'Franck Paul',
+        '4.1',
+        [
+            'date'     => '2003-08-13T13:42:00+0100',
+            'requires' => [['core', '2.36']],
+            'type'     => 'theme',
+            'tplset'   => 'dotty',
+
+            'details'    => 'https://open-time.net/?q=ensemble',
+            'support'    => 'https://github.com/franck-paul/ensemble',
+            'repository' => 'https://raw.githubusercontent.com/franck-paul/ensemble/main/dcstore.xml',
+            'license'    => 'gpl2',
+        ]
+    );
+}
